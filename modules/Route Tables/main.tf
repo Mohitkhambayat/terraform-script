68a64f7258148e0998ae95c0a26dcdc8694a0b90
Mohit <48855847+Mohitkhambayat@users.noreply.github.com>
2026-03-23 23:18:05 +0530

    Update main.tf

--- modules/Route Tables/main.tf
+++ modules/Route Tables/main.tf
@@ -1,1 +1,32 @@
+resource "aws_route_table" "public" {
+  vpc_id = var.vpc_id
+}
 
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = var.igw_id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count = length(var.public_subnet_ids)
+  subnet_id      = var.public_subnet_ids[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table" "private" {
+  vpc_id = var.vpc_id
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = var.nat_gateway_id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count = length(var.private_subnet_ids)
+  subnet_id      = var.private_subnet_ids[count.index]
+  route_table_id = aws_route_table.private.id
+}
